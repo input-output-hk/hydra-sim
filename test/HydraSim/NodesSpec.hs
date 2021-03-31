f3c6552ab9e18aa33aa7c91e5b36d2697a59fa2a
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-03-31 16:30:08 +0000

    Test simple simulation run confirms all transactions

--- test/HydraSim/NodesSpec.hs
+++ test/HydraSim/NodesSpec.hs
@@ -1,8 +1,16 @@
 module HydraSim.NodesSpec where
 
+import HydraSim.Analyse
+import HydraSim.Options
+import HydraSim.Run
 import Test.Hspec
 
 spec :: Spec
-spec = describe "Hydra Simulator" $ do
-    it "correctly confirms transactions and snapshots" $ do
-        pendingWith "not implemented"
+spec = describe "Hydra Simulation with 3 nodes" $ do
+    it "correctly confirms all transactions" $ do
+        let capacity :: Int
+            capacity = 10
+
+            traceRun = runSimulation defaultOptions capacity
+
+        length (confirmedTxs (selectTraceHydraEvents DontShowDebugMessages traceRun)) `shouldBe` fromIntegral (numberTxs defaultOptions * 3)
