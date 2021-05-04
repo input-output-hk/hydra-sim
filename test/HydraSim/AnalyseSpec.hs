3491b33370d3e971dc3e1a3bf4a6f4c53b2571ff
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-05-04 13:36:32 +0000

    Failing test computing average

--- test/HydraSim/AnalyseSpec.hs
+++ test/HydraSim/AnalyseSpec.hs
@@ -1,5 +1,7 @@
 module HydraSim.AnalyseSpec where
 
+import Control.Monad.Class.MonadTime (Time (Time))
+import Control.Monad.IOSim (ThreadLabel)
 import HydraSim.Analyse
 import Test.Hspec
 
@@ -8,3 +10,11 @@ spec = describe "Analyse Simulation" $ do
     describe "Average Confirmation Time" $ do
         it "returns Nothing given no transaction" $ do
             avgConfTime [] `shouldBe` Nothing
+        it "returns Nothing given no transaction is confirmed" $ do
+            avgConfTime [TxUnconfirmed someLabel someTime] `shouldBe` Nothing
+
+someTime :: Time
+someTime = Time 10000
+
+someLabel :: ThreadLabel
+someLabel = "thread"
