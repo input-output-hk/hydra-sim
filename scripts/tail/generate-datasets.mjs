25fae7bdff1c92ae24b3ecc57dd4b9367b6771b2
KtorZ <matthias.benkort@gmail.com>
2021-06-08 17:45:22 +0200

    Adjust matrix: lower down payment window by a factor of 100, and increase the default number of clients to 5000

--- scripts/tail/generate-datasets.mjs
+++ scripts/tail/generate-datasets.mjs
@@ -3,34 +3,29 @@ import { spawn } from 'child_process';
 const CONCURRENCY = 6;
 
 const matrix =
-  [ { numberOfClients: 1000,  compression: 1000,  maxSlot: 591 }
-  , { numberOfClients: 1000,  compression: 2500,  maxSlot: 591 }
-  , { numberOfClients: 1000,  compression: 5000,  maxSlot: 591 }
-  , { numberOfClients: 1000,  compression: 10000, maxSlot: 591 }
-  , { numberOfClients: 1000,  compression: 11000, maxSlot: 591 }
-  , { numberOfClients: 1000,  compression: 12000, maxSlot: 591 }
-  , { numberOfClients: 1000,  compression: 13000, maxSlot: 591 }
-  , { numberOfClients: 1000,  compression: 14000, maxSlot: 591 }
-  , { numberOfClients: 1000,  compression: 15000, maxSlot: 591 }
-  , { numberOfClients: 1000,  compression: 16000, maxSlot: 591 }
-  , { numberOfClients: 1000,  compression: 17000, maxSlot: 591 }
-  , { numberOfClients: 1000,  compression: 18000, maxSlot: 591 }
-  , { numberOfClients: 1000,  compression: 19000, maxSlot: 591 }
-  , { numberOfClients: 1000,  compression: 20000, maxSlot: 591 }
-  , { numberOfClients: 2000,  compression: 20000, maxSlot: 591 }
-  , { numberOfClients: 3000,  compression: 20000, maxSlot: 591 }
-  , { numberOfClients: 4000,  compression: 20000, maxSlot: 591 }
+  [ { numberOfClients: 5000,  compression: 1000,  maxSlot: 591 }
+  , { numberOfClients: 5000,  compression: 2500,  maxSlot: 591 }
+  , { numberOfClients: 5000,  compression: 5000,  maxSlot: 591 }
+  , { numberOfClients: 5000,  compression: 10000, maxSlot: 591 }
+  , { numberOfClients: 5000,  compression: 11000, maxSlot: 591 }
+  , { numberOfClients: 5000,  compression: 12000, maxSlot: 591 }
+  , { numberOfClients: 5000,  compression: 13000, maxSlot: 591 }
+  , { numberOfClients: 5000,  compression: 14000, maxSlot: 591 }
+  , { numberOfClients: 5000,  compression: 15000, maxSlot: 591 }
+  , { numberOfClients: 5000,  compression: 16000, maxSlot: 591 }
+  , { numberOfClients: 5000,  compression: 17000, maxSlot: 591 }
+  , { numberOfClients: 5000,  compression: 18000, maxSlot: 591 }
+  , { numberOfClients: 5000,  compression: 19000, maxSlot: 591 }
   , { numberOfClients: 5000,  compression: 20000, maxSlot: 591 }
-  , { numberOfClients: 6000,  compression: 20000, maxSlot: 591 }
-  , { numberOfClients: 7000,  compression: 20000, maxSlot: 591 }
-  , { numberOfClients: 8000,  compression: 20000, maxSlot: 591 }
-  , { numberOfClients: 9000,  compression: 20000, maxSlot: 591 }
-  , { numberOfClients: 10000, compression: 20000, maxSlot: 591 }
-  , { numberOfClients: 11000, compression: 20000, maxSlot: 591 }
-  , { numberOfClients: 12000, compression: 20000, maxSlot: 591 }
-  , { numberOfClients: 13000, compression: 20000, maxSlot: 591 }
-  , { numberOfClients: 14000, compression: 20000, maxSlot: 591 }
-  , { numberOfClients: 15000, compression: 20000, maxSlot: 591 }
+  , { numberOfClients: 1000,  compression: 20000, maxSlot: 591 }
+  , { numberOfClients: 1500,  compression: 20000, maxSlot: 591 }
+  , { numberOfClients: 2000,  compression: 20000, maxSlot: 591 }
+  , { numberOfClients: 2500,  compression: 20000, maxSlot: 591 }
+  , { numberOfClients: 3000,  compression: 20000, maxSlot: 591 }
+  , { numberOfClients: 3500,  compression: 20000, maxSlot: 591 }
+  , { numberOfClients: 4000,  compression: 20000, maxSlot: 591 }
+  , { numberOfClients: 4500,  compression: 20000, maxSlot: 591 }
+  , { numberOfClients: 5000,  compression: 20000, maxSlot: 591 }
   ]
 
 for (let i = 0; i < (matrix.length + CONCURRENCY); i += CONCURRENCY) {
--- scripts/tail/run-simulations.mjs
+++ scripts/tail/run-simulations.mjs
@@ -7,63 +7,56 @@ const __dirname = path.resolve();
 
 const matrix =
   // Compression / Density
-  [ { "window": 10000, "delay": 120, numberOfClients: 1000,  compression:  1000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 1000,  compression:  2500 }
-  , { "window": 10000, "delay": 120, numberOfClients: 1000,  compression:  5000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 1000,  compression: 10000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 1000,  compression: 11000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 1000,  compression: 12000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 1000,  compression: 13000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 1000,  compression: 14000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 1000,  compression: 15000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 1000,  compression: 16000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 1000,  compression: 17000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 1000,  compression: 18000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 1000,  compression: 19000 }
-  // , { "window": 10000, "delay": 120, numberOfClients: 1000,  compression: 20000 }
+  [ { "window": 100, "delay": 120, numberOfClients: 5000,  compression:  1000 }
+  , { "window": 100, "delay": 120, numberOfClients: 5000,  compression:  2500 }
+  , { "window": 100, "delay": 120, numberOfClients: 5000,  compression:  5000 }
+  , { "window": 100, "delay": 120, numberOfClients: 5000,  compression: 10000 }
+  , { "window": 100, "delay": 120, numberOfClients: 5000,  compression: 11000 }
+  , { "window": 100, "delay": 120, numberOfClients: 5000,  compression: 12000 }
+  , { "window": 100, "delay": 120, numberOfClients: 5000,  compression: 13000 }
+  , { "window": 100, "delay": 120, numberOfClients: 5000,  compression: 14000 }
+  , { "window": 100, "delay": 120, numberOfClients: 5000,  compression: 15000 }
+  , { "window": 100, "delay": 120, numberOfClients: 5000,  compression: 16000 }
+  , { "window": 100, "delay": 120, numberOfClients: 5000,  compression: 17000 }
+  , { "window": 100, "delay": 120, numberOfClients: 5000,  compression: 18000 }
+  , { "window": 100, "delay": 120, numberOfClients: 5000,  compression: 19000 }
+  , { "window": 100, "delay": 120, numberOfClients: 5000,  compression: 20000 }
 
   // Settlement Delay
-  , { "window": 10000, "delay":  30, numberOfClients: 1000,  compression: 20000 }
-  , { "window": 10000, "delay":  60, numberOfClients: 1000,  compression: 20000 }
-  , { "window": 10000, "delay":  90, numberOfClients: 1000,  compression: 20000 }
-  // , { "window":  10000, "delay": 120, numberOfClients: 1000,  compression: 20000 }
-  , { "window": 10000, "delay": 150, numberOfClients: 1000,  compression: 20000 }
-  , { "window": 10000, "delay": 180, numberOfClients: 1000,  compression: 20000 }
-  , { "window": 10000, "delay": 210, numberOfClients: 1000,  compression: 20000 }
-  , { "window": 10000, "delay": 240, numberOfClients: 1000,  compression: 20000 }
-  , { "window": 10000, "delay": 270, numberOfClients: 1000,  compression: 20000 }
-  , { "window": 10000, "delay": 300, numberOfClients: 1000,  compression: 20000 }
-  , { "window": 10000, "delay": 450, numberOfClients: 1000,  compression: 20000 }
-  , { "window": 10000, "delay": 600, numberOfClients: 1000,  compression: 20000 }
+  , { "window": 100, "delay":  30, numberOfClients: 5000,  compression: 20000 }
+  , { "window": 100, "delay":  60, numberOfClients: 5000,  compression: 20000 }
+  , { "window": 100, "delay":  90, numberOfClients: 5000,  compression: 20000 }
+  // , { "window":  100, "delay": 120, numberOfClients: 5000,  compression: 20000 }
+  , { "window": 100, "delay": 150, numberOfClients: 5000,  compression: 20000 }
+  , { "window": 100, "delay": 180, numberOfClients: 5000,  compression: 20000 }
+  , { "window": 100, "delay": 210, numberOfClients: 5000,  compression: 20000 }
+  , { "window": 100, "delay": 240, numberOfClients: 5000,  compression: 20000 }
+  , { "window": 100, "delay": 270, numberOfClients: 5000,  compression: 20000 }
+  , { "window": 100, "delay": 300, numberOfClients: 5000,  compression: 20000 }
 
   // Payment Window
-  , { "window":   1000, "delay": 120, numberOfClients: 1000,  compression: 20000 }
-  , { "window":   2500, "delay": 120, numberOfClients: 1000,  compression: 20000 }
-  , { "window":   5000, "delay": 120, numberOfClients: 1000,  compression: 20000 }
-  , { "window":   7500, "delay": 120, numberOfClients: 1000,  compression: 20000 }
-  // , { "window":  10000, "delay": 120, numberOfClients: 1000,  compression: 20000 }
-  , { "window":  12500, "delay": 120, numberOfClients: 1000,  compression: 20000 }
-  , { "window":  15000, "delay": 120, numberOfClients: 1000,  compression: 20000 }
-  , { "window":  20000, "delay": 120, numberOfClients: 1000,  compression: 20000 }
-  , { "window":  25000, "delay": 120, numberOfClients: 1000,  compression: 20000 }
-  , { "window":  50000, "delay": 120, numberOfClients: 1000,  compression: 20000 }
+  , { "window":   50, "delay": 120, numberOfClients: 5000,  compression: 20000 }
+  // , { "window":   100, "delay": 120, numberOfClients: 5000,  compression: 20000 }
+  , { "window":  200, "delay": 120, numberOfClients: 5000,  compression: 20000 }
+  , { "window":  300, "delay": 120, numberOfClients: 5000,  compression: 20000 }
+  , { "window":  400, "delay": 120, numberOfClients: 5000,  compression: 20000 }
+  , { "window":  500, "delay": 120, numberOfClients: 5000,  compression: 20000 }
+  , { "window":  600, "delay": 120, numberOfClients: 5000,  compression: 20000 }
+  , { "window":  700, "delay": 120, numberOfClients: 5000,  compression: 20000 }
+  , { "window":  800, "delay": 120, numberOfClients: 5000,  compression: 20000 }
+  , { "window":  900, "delay": 120, numberOfClients: 5000,  compression: 20000 }
+  , { "window": 1000, "delay": 120, numberOfClients: 5000,  compression: 20000 }
 
   // Number of Clients
-  , { "window": 10000, "delay": 120, numberOfClients:  1000, compression: 20000 }
-  , { "window": 10000, "delay": 120, numberOfClients:  2000, compression: 20000 }
-  , { "window": 10000, "delay": 120, numberOfClients:  3000, compression: 20000 }
-  , { "window": 10000, "delay": 120, numberOfClients:  4000, compression: 20000 }
-  , { "window": 10000, "delay": 120, numberOfClients:  5000, compression: 20000 }
-  , { "window": 10000, "delay": 120, numberOfClients:  6000, compression: 20000 }
-  , { "window": 10000, "delay": 120, numberOfClients:  7000, compression: 20000 }
-  , { "window": 10000, "delay": 120, numberOfClients:  8000, compression: 20000 }
-  , { "window": 10000, "delay": 120, numberOfClients:  9000, compression: 20000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 10000, compression: 20000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 11000, compression: 20000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 12000, compression: 20000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 13000, compression: 20000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 14000, compression: 20000 }
-  , { "window": 10000, "delay": 120, numberOfClients: 15000, compression: 20000 }
+  , { "window": 100, "delay": 120, numberOfClients:  1000, compression: 20000 }
+  , { "window": 100, "delay": 120, numberOfClients:  1500, compression: 20000 }
+  , { "window": 100, "delay": 120, numberOfClients:  2000, compression: 20000 }
+  , { "window": 100, "delay": 120, numberOfClients:  2500, compression: 20000 }
+  , { "window": 100, "delay": 120, numberOfClients:  3000, compression: 20000 }
+  , { "window": 100, "delay": 120, numberOfClients:  3500, compression: 20000 }
+  , { "window": 100, "delay": 120, numberOfClients:  4000, compression: 20000 }
+  , { "window": 100, "delay": 120, numberOfClients:  4500, compression: 20000 }
+  // , { "window": 100, "delay": 120, numberOfClients:  5000, compression: 20000 }
   ]
 
 let cursor = 0;
@@ -79,7 +72,7 @@ async function schedule() {
     console.log(`Running simulation ${JSON.stringify(matrix[cursor])}`);
     cursor += 1;
     const filename = `events-clients:${numberOfClients}-compression:${compression}`;
-    const writer = fs.createWriteStream(path.join(__dirname, "..", "..", `${filename}-window:${window}-delay:${delay}-clients:${numberOfClients}`));
+    const writer = fs.createWriteStream(path.join(__dirname, "..", "..", `${filename}-window:${window}-delay:${delay}`));
     const pipeline = spawn("hydra-tail-simulation", [ "run"
       , "--payment-window", window
       , "--settlement-delay", delay
