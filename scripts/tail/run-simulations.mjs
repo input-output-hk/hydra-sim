5ce284d078755a8cfcf7e09081256a1677123af0
KtorZ <matthias.benkort@gmail.com>
2021-09-13 18:21:41 +0200

    Adjust the simulation matrix with the new parameters of interest.

--- scripts/tail/run-simulations.mjs
+++ scripts/tail/run-simulations.mjs
@@ -6,41 +6,25 @@ const CONCURRENCY = 16;
 const __dirname = path.resolve();
 
 const matrix =
-  // Settlement Delay
-  [ { "window": 100, "delay":  30, numberOfClients: 5000,  compression: 20000 }
-  , { "window": 100, "delay":  60, numberOfClients: 5000,  compression: 20000 }
-  , { "window": 100, "delay":  90, numberOfClients: 5000,  compression: 20000 }
-  , { "window": 100, "delay": 120, numberOfClients: 5000,  compression: 20000 }
-  , { "window": 100, "delay": 150, numberOfClients: 5000,  compression: 20000 }
-  , { "window": 100, "delay": 180, numberOfClients: 5000,  compression: 20000 }
-  , { "window": 100, "delay": 210, numberOfClients: 5000,  compression: 20000 }
-  , { "window": 100, "delay": 240, numberOfClients: 5000,  compression: 20000 }
-  , { "window": 100, "delay": 270, numberOfClients: 5000,  compression: 20000 }
-  , { "window": 100, "delay": 300, numberOfClients: 5000,  compression: 20000 }
+  [ { "window": 100, "delay": 600, "numberOfClients": 1000, "compression": 4320, "cutOff": 1.0, "proActive": 1.0 }
+  , { "window": 100, "delay": 600, "numberOfClients": 1000, "compression": 4320, "cutOff": 0.9, "proActive": 1.0 }
+  , { "window": 100, "delay": 600, "numberOfClients": 1000, "compression": 4320, "cutOff": 0.8, "proActive": 1.0 }
+  , { "window": 100, "delay": 600, "numberOfClients": 1000, "compression": 4320, "cutOff": 0.7, "proActive": 1.0 }
+  , { "window": 100, "delay": 600, "numberOfClients": 1000, "compression": 4320, "cutOff": 0.6, "proActive": 1.0 }
+  , { "window": 100, "delay": 600, "numberOfClients": 1000, "compression": 4320, "cutOff": 0.5, "proActive": 1.0 }
 
-  // Payment Window
-  , { "window":   50, "delay": 120, numberOfClients: 5000,  compression: 20000 }
-  // , { "window":   100, "delay": 120, numberOfClients: 5000,  compression: 20000 }
-  , { "window":  200, "delay": 120, numberOfClients: 5000,  compression: 20000 }
-  , { "window":  300, "delay": 120, numberOfClients: 5000,  compression: 20000 }
-  , { "window":  400, "delay": 120, numberOfClients: 5000,  compression: 20000 }
-  , { "window":  500, "delay": 120, numberOfClients: 5000,  compression: 20000 }
-  , { "window":  600, "delay": 120, numberOfClients: 5000,  compression: 20000 }
-  , { "window":  700, "delay": 120, numberOfClients: 5000,  compression: 20000 }
-  , { "window":  800, "delay": 120, numberOfClients: 5000,  compression: 20000 }
-  , { "window":  900, "delay": 120, numberOfClients: 5000,  compression: 20000 }
-  , { "window": 1000, "delay": 120, numberOfClients: 5000,  compression: 20000 }
+  , { "window": 100, "delay": 600, "numberOfClients": 1000, "compression": 4320, "cutOff": 1.0, "proActive": 0.9 }
+  , { "window": 100, "delay": 600, "numberOfClients": 1000, "compression": 4320, "cutOff": 1.0, "proActive": 0.8 }
+  , { "window": 100, "delay": 600, "numberOfClients": 1000, "compression": 4320, "cutOff": 1.0, "proActive": 0.7 }
+  , { "window": 100, "delay": 600, "numberOfClients": 1000, "compression": 4320, "cutOff": 1.0, "proActive": 0.6 }
+  , { "window": 100, "delay": 600, "numberOfClients": 1000, "compression": 4320, "cutOff": 1.0, "proActive": 0.5 }
 
-  // Number of Clients
-  , { "window": 100, "delay": 120, numberOfClients:  1000, compression: 20000 }
-  , { "window": 100, "delay": 120, numberOfClients:  1500, compression: 20000 }
-  , { "window": 100, "delay": 120, numberOfClients:  2000, compression: 20000 }
-  , { "window": 100, "delay": 120, numberOfClients:  2500, compression: 20000 }
-  , { "window": 100, "delay": 120, numberOfClients:  3000, compression: 20000 }
-  , { "window": 100, "delay": 120, numberOfClients:  3500, compression: 20000 }
-  , { "window": 100, "delay": 120, numberOfClients:  4000, compression: 20000 }
-  , { "window": 100, "delay": 120, numberOfClients:  4500, compression: 20000 }
-  // , { "window": 100, "delay": 120, numberOfClients:  5000, compression: 20000 }
+  , { "window": 100, "delay": 900, "numberOfClients": 1000, "compression": 4320, "cutOff": 1.0, "proActive": 1.0 }
+  , { "window": 100, "delay": 800, "numberOfClients": 1000, "compression": 4320, "cutOff": 1.0, "proActive": 1.0 }
+  , { "window": 100, "delay": 700, "numberOfClients": 1000, "compression": 4320, "cutOff": 1.0, "proActive": 1.0 }
+  , { "window": 100, "delay": 500, "numberOfClients": 1000, "compression": 4320, "cutOff": 1.0, "proActive": 1.0 }
+  , { "window": 100, "delay": 400, "numberOfClients": 1000, "compression": 4320, "cutOff": 1.0, "proActive": 1.0 }
+  , { "window": 100, "delay": 300, "numberOfClients": 1000, "compression": 4320, "cutOff": 1.0, "proActive": 1.0 }
   ]
 
 let cursor = 0;
@@ -52,14 +36,16 @@ await Promise.all(pipeline);
 
 async function schedule() {
   if (matrix[cursor]) {
-    const { window, delay, numberOfClients, compression } = matrix[cursor];
+    const { window, delay, numberOfClients, compression, cutOff, proActive } = matrix[cursor];
     console.log(`Running simulation ${JSON.stringify(matrix[cursor])}`);
     cursor += 1;
     const filename = `events-clients:${numberOfClients}-compression:${compression}`;
-    const writer = fs.createWriteStream(path.join(__dirname, "..", "..", `${filename}-window:${window}-delay:${delay}`));
+    const writer = fs.createWriteStream(path.join(__dirname, "..", "..", `${filename}-window:${window}-delay:${delay}-proActive:${proActive}-cutOff:${cutOff}`));
     const pipeline = spawn("hydra-tail-simulation", [ "run"
       , "--payment-window", window
       , "--settlement-delay", delay
+      , "--pro-active-snapshot", proActive
+      , "--payment-cut-off", cutOff
       , path.join(__dirname, "datasets", `${filename}.csv`)
       ])
     const promise = new Promise((resolve) => {
